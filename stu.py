5e361dc9862c82bb06fe2b28d5d05a04b808e3a0
Ratna <ratnavk74@gmail.com>
2025-10-30 15:45:57 +0530

    Create stu.py

---
+++ stu.py
@@ -0,0 +1,11 @@
+
+name = input("Enter student's name: ")
+age = input("Enter student's age: ")
+usn = input("Enter student's USN: ")
+address = input("Enter student's address: ")
+
+print("\n--- Student Details ---")
+print("Name     :", name)
+print("Age      :", age)
+print("USN      :", usn)
+print("Address  :", address)
